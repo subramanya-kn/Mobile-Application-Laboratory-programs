518474c3ef5caa1399d79e97ed115390856dd57b
subbu14689 <subramanyakn2002@gmail.com>
2022-07-24 19:00:06 +0530

    MAD

---
+++ Counter/app/src/main/java/in/edu/cambridge/counter/MainActivity.kt
@@ -0,0 +1,66 @@
+package `in`.edu.cambridge.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.os.CountDownTimer
+//import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var counterValue:TextView
+    private lateinit var startCounter : Button
+    private lateinit var stopCounter : Button
+    private lateinit var resetCounter : Button
+
+    val timer = MyCounter(10000000, 10)
+    var  countervalue : Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        counterValue = findViewById(R.id.counter_value)
+        startCounter = findViewById(R.id.start_counter)
+
+        startCounter.setOnClickListener {
+            timer.start()
+            startCounter.isEnabled=false
+        }
+        stopCounter = findViewById(R.id.stop_counter)
+        stopCounter.setOnClickListener {
+            timer.cancel()
+            startCounter.isEnabled=true
+        }
+        resetCounter = findViewById(R.id.reset_counter)
+        resetCounter.setOnClickListener {
+
+            countervalue = 0
+            counterValue.text = (countervalue).toString()
+
+        }
+        /*fun reset(view : View){
+               val count = 0
+               var text = count.toString()
+           }*/
+
+
+
+
+    }
+
+    inner class MyCounter(millisInFuture: Long, countDownInterval: Long) : CountDownTimer(millisInFuture, countDownInterval) {
+        override fun onFinish() {
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            countervalue++
+            counterValue.text = (countervalue).toString()
+
+
+        }
+
+
+    }
+
+}
